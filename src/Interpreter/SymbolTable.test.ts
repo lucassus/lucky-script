6219cebe938f7e19611011bdd4a13c186f5a106b
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-09 09:49:30 +0200

    Add support for scopes

--- src/Interpreter/SymbolTable.test.ts
+++ src/Interpreter/SymbolTable.test.ts
@@ -10,4 +10,28 @@ describe("SymbolTable", () => {
 
     expect(symbolTable.has("y")).toBe(false);
   });
+
+  it("can access variables from the parent scope", () => {
+    const root = new SymbolTable();
+    root.set("a", 1);
+
+    const child = new SymbolTable(root);
+
+    expect(child.has("a")).toBe(true);
+    expect(child.get("a")).toBe(1);
+  });
+
+  it("can set variables on the parent scope", () => {
+    const grandFather = new SymbolTable();
+    const father = new SymbolTable();
+    father.set("a", 1);
+    const child = new SymbolTable(father);
+
+    child.set("a", 2);
+
+    expect(child.has("a")).toBe(true);
+    expect(father.has("a")).toBe(true);
+    expect(father.get("a")).toBe(2);
+    expect(grandFather.has("a")).toBe(false);
+  });
 });
--- src/Interpreter/SymbolTable.ts
+++ src/Interpreter/SymbolTable.ts
@@ -5,15 +5,31 @@ export type MyObject = number | FunctionDeclaration;
 export class SymbolTable {
   private map: Map<string, MyObject> = new Map();
 
+  constructor(private parent?: SymbolTable) {}
+
   set(key: string, value: MyObject): void {
-    this.map.set(key, value);
+    let curr: SymbolTable = this;
+
+    while (curr.parent !== undefined && !curr.map.has("key")) {
+      curr = curr.parent;
+    }
+
+    curr.map.set(key, value);
   }
 
   get(key: string): undefined | MyObject {
-    return this.map.get(key);
+    if (this.map.has(key)) {
+      return this.map.get(key);
+    }
+
+    return this.parent ? this.parent.get(key) : undefined;
   }
 
   has(key: string): boolean {
-    return this.map.has(key);
+    if (this.map.has(key)) {
+      return true;
+    }
+
+    return this.parent ? this.parent.has(key) : false;
   }
 }
