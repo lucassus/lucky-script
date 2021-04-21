bae29baea73b7f9316ec8238822a42b9abf94aa4
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-21 20:39:26 +0200

    Improve blocks

--- lark-sandbox/lucky_script_test.py
+++ lark-sandbox/lucky_script_test.py
@@ -3,7 +3,7 @@ from lark import Lark
 
 if __name__ == "__main__":
     grammar = open(os.path.join(os.path.dirname(__file__), "lucky_script.lark"), "r").read()
-    parser = Lark(grammar, start="block", parser="lalr")
+    parser = Lark(grammar, start="program", parser="lalr")
 
     script = """
     x = 1
