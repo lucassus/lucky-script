68899468fc0a6164ca15ea3a3f6a80d6749baca3
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-21 17:45:58 +0200

    Add simple functions

--- lark-sandbox/lucky_script_test.py
+++ lark-sandbox/lucky_script_test.py
@@ -3,13 +3,26 @@ from lark import Lark
 
 if __name__ == "__main__":
     grammar = open(os.path.join(os.path.dirname(__file__), "lucky_script.lark"), "r").read()
-    parser = Lark(grammar, start="program", parser="lalr")
+    parser = Lark(grammar, start="block", parser="lalr")
 
     script = """
     x = 1
 
+    1 + 2 * 3
 
-    y = -1 + 0.9999 * (3.5 + --+-4) ** 2
+    y = -1 + 0.9999 * (3.5 + --+-4) ** 2 + x
+    
+    function add() { 1 + 2 }
+    
+    function foo() { 
+      x = 1
+      y = 2
+      1 + 2 * 3 + -y ** -x
+    }
+    
+    function bar() {}
+    
+    add()
     """
 
     ast = parser.parse(script)
