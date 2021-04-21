770a99b7f5fcc3b76f19ae2784e56ed2df4e892b
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-21 17:37:09 +0200

    Add statements

--- lark-sandbox/lucky_script_test.py
+++ lark-sandbox/lucky_script_test.py
@@ -3,13 +3,15 @@ from lark import Lark
 
 if __name__ == "__main__":
     grammar = open(os.path.join(os.path.dirname(__file__), "lucky_script.lark"), "r").read()
-    parser = Lark(grammar, start="expression", parser="lalr")
+    parser = Lark(grammar, start="program", parser="lalr")
 
-    # TODO: Build a simple calculator
-    # TODO: Figure how to build a pretty AST
     script = """
-    x = y = -1 + 0.9999 * (3.5 + --+-4) ** 2
+    x = 1
+
+
+    y = -1 + 0.9999 * (3.5 + --+-4) ** 2
     """
+
     ast = parser.parse(script)
 
     print(ast.pretty())
