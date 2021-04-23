7be7bd53e4e9535e86dd79bfd9bbce7fdf7d455f
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-23 07:27:30 +0200

    Update grammar for json

--- lark-sandbox/json_test.py
+++ lark-sandbox/json_test.py
@@ -5,14 +5,16 @@ if __name__ == "__main__":
     grammar = open(os.path.join(os.path.dirname(__file__), "json.lark"), "r").read()
     parser = Lark(grammar, start="value")
 
-    # json = """
-    # {
-    #   "key": ["item0", "item1", 3.14,
-    #   { "foo": "bar", "bar": 123 }],
-    #   "one": { "two": { "three": 3 } }
-    # }
-    # """
-    json = open(os.path.join(os.path.dirname(__file__), "../package.json"), "r").read()
+    json = """
+    {
+      "key": ["item0", "item1", 3.14,
+      { "foo": "bar", "bar": 123 }],
+      "one": { "two": { "three": 3 } },
+      "empty object": {},
+      "empty list": []
+    }
+    """
+    # json = open(os.path.join(os.path.dirname(__file__), "../package.json"), "r").read()
     ast = parser.parse(json)
 
     print(ast.pretty())
