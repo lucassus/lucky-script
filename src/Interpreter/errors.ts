8861112c2c3942e2d22640f8fbb7c0220ef40777
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-04-29 11:16:58 +0200

    Set valid prototypes for errors

--- src/Interpreter/errors.ts
+++ src/Interpreter/errors.ts
@@ -1,17 +1,20 @@
 export class RuntimeError extends Error {
   constructor(message: string) {
     super(message);
+    Object.setPrototypeOf(this, RuntimeError.prototype);
   }
 }
 
 export class NameError extends RuntimeError {
   constructor(name: string) {
     super(`Identifier ${name} is not defined`);
+    Object.setPrototypeOf(this, NameError.prototype);
   }
 }
 
 export class ZeroDivisionError extends RuntimeError {
   constructor() {
     super("Division by zero");
+    Object.setPrototypeOf(this, ZeroDivisionError.prototype);
   }
 }
