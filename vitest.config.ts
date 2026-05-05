3d1852d704615c23f62c099f40ee61a5d9de5a2c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2026-05-05 11:49:19 +0200

    Setup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts"],
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts", "src/repl.ts"],
+    },
   },
 });
