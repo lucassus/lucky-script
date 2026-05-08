4a80fbc261ef7b313b9135c6f67df83a186f9553
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2026-05-08 11:24:08 +0200

    Refactor tryConsumeSymbol

--- src/Lexer/Lexer.ts
+++ src/Lexer/Lexer.ts
@@ -73,41 +73,41 @@ export class Lexer {
 
       case "+":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.PlusAssign : Operator.Plus,
+          this.tryConsumeSymbol("=") ? Operator.PlusAssign : Operator.Plus,
         );
       case "-":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.MinusAssign : Operator.Minus,
+          this.tryConsumeSymbol("=") ? Operator.MinusAssign : Operator.Minus,
         );
       case "*":
-        if (this.nextSymbolMatches("*")) {
+        if (this.tryConsumeSymbol("*")) {
           return this.createToken(Operator.Power);
         }
         return this.createToken(
-          this.nextSymbolMatches("=")
+          this.tryConsumeSymbol("=")
             ? Operator.MultiplyAssign
             : Operator.Multiply,
         );
       case "/":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.DivideAssign : Operator.Divide,
+          this.tryConsumeSymbol("=") ? Operator.DivideAssign : Operator.Divide,
         );
       case "<":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.Lte : Operator.Lt,
+          this.tryConsumeSymbol("=") ? Operator.Lte : Operator.Lt,
         );
       case "=":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.Eq : Operator.Assigment,
+          this.tryConsumeSymbol("=") ? Operator.Eq : Operator.Assigment,
         );
       case "!":
-        if (!this.nextSymbolMatches("=")) {
+        if (!this.tryConsumeSymbol("=")) {
           throw new IllegalSymbolError(this.currentSymbol, this.position);
         }
         return this.createToken(Operator.Neq);
       case ">":
         return this.createToken(
-          this.nextSymbolMatches("=") ? Operator.Gte : Operator.Gt,
+          this.tryConsumeSymbol("=") ? Operator.Gte : Operator.Gt,
         );
 
       case '"':
@@ -148,7 +148,7 @@ export class Lexer {
     return this.input[this.position + 1];
   }
 
-  private nextSymbolMatches(symbol: string): boolean {
+  private tryConsumeSymbol(symbol: string): boolean {
     if (this.nextSymbol === symbol) {
       this.advance();
       return true;
