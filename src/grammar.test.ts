b8544cdb38fdf7709a9d267ee0db466aa4b5a924
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2026-05-10 16:04:20 +0200

    Simple grammar example

--- src/grammar.test.ts
+++ src/grammar.test.ts
@@ -1,22 +1,63 @@
 import * as ohm from "ohm-js";
 import { describe, expect, it } from "vitest";
 
-
-describe("grammar", ()=> {
-  it("test", ()=> {
+describe("grammar", () => {
+  it("MyGrammar", () => {
     const grammar = ohm.grammar(String.raw`
-          MyGrammar {
-            greeting = "Hello" | "Hola"
-          }
+      MyGrammar {
+        greeting = "Hello" | "Hola"
+      }
     `);
 
-    let m = grammar.match("Hello");
-    expect(m.succeeded()).toBe(true);
+    let result = grammar.match("Hello");
+    expect(result.succeeded()).toBe(true);
 
-    m = grammar.match("Hola");
-    expect(m.succeeded()).toBe(true);
+    result = grammar.match("Hola");
+    expect(result.succeeded()).toBe(true);
 
-    m = grammar.match("Hi");
-    expect(m.succeeded()).toBe(false);
+    result = grammar.match("Hi");
+    expect(result.succeeded()).toBe(false);
+  });
+
+  it("Arithmetics", () => {
+    const grammar = ohm.grammar(`
+      Arithmetic {
+        Exp = AddExp
+        
+        /* 
+          Handles low-precedence operations
+          The rule is defined "above" multiplication, because it call MulExp, 
+          the parser ensures that multiplication is grouped more tightly than addition
+        */
+        AddExp = AddExp "+" MulExp -- plus
+          | AddExp "-" MulExp -- minus
+          | MulExp
+
+        // Handles high-precedence operations
+        MulExp = MulExp "*" number -- times
+          | MulExp "/" number -- div
+          | PriExp
+          
+        PriExp = "(" Exp ")" -- paren
+         | number
+
+        number = digit*
+      }
+    `);
+
+    let result = grammar.match("42");
+    expect(result.succeeded()).toBe(true);
+
+    result = grammar.match("1");
+    expect(result.succeeded()).toBe(true);
+
+    result = grammar.match("1 + 2 * 3");
+    expect(result.succeeded()).toBe(true);
+
+    result = grammar.match("99 - 2 + 1");
+    expect(result.succeeded()).toBe(true);
+
+    result = grammar.match("(99 - 2) / 2");
+    expect(result.succeeded()).toBe(true);
   });
 });
